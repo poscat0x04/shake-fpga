f58b9aa0c6c4d1b429b828a2fe57935d5d9e5a75
poscat <poscat@poscat.moe>
2024-12-29 13:44:31 +0800

    move the program logic to library instead

--- app/Main.hs
+++ app/Main.hs
@@ -2,35 +2,7 @@
 
 module Main where
 
-import Development.Shake.FPGA (shakeFromFile)
-import Options.Applicative
-import System.Environment (withArgs)
+import Development.Shake.FPGA (shakeMain)
 
 main :: IO ()
-main = do
-  (config, rest) <- execParser opts
-  withArgs rest $ shakeFromFile config
-  where
-    parser = do
-      config <-
-        strOption
-          ( metavar "CONFIG"
-              <> long "config"
-              <> short 'c'
-              <> help "The configuration file to use (default: shake-fpga.yaml)"
-              <> value "shake-fpga.yaml"
-          )
-      rest <-
-        many
-          ( strArgument
-              ( metavar "ARGS"
-                  <> help "arguments to pass to shake (use -- to pass arguments with leading -)"
-              )
-          )
-      pure (config, rest)
-    opts =
-      info
-        (parser <**> helper)
-        ( fullDesc
-            <> progDesc "Build clash based FPGA projects using shake"
-        )
+main = shakeMain
--- src/Development/Shake/FPGA.hs
+++ src/Development/Shake/FPGA.hs
@@ -1,5 +1,13 @@
 module Development.Shake.FPGA
-  ( module Development.Shake.FPGA.Internal,
+  ( -- * Program as a library
+
+    -- | Since clash requires the clash-prelude package used to compile clash
+    --   and used in clash designs to be the exact same package, you can include
+    --   this library in your cabal file to ensure they are the same.
+    shakeMain,
+
+    -- * Re-exports
+    module Development.Shake.FPGA.Internal,
   )
 where
 
@@ -12,3 +20,34 @@ import Development.Shake.FPGA.Internal
     rulesFromFile,
     shakeFromFile,
   )
+import Options.Applicative
+import System.Environment (withArgs)
+
+shakeMain :: IO ()
+shakeMain = do
+  (config, rest) <- execParser opts
+  withArgs rest $ shakeFromFile config
+  where
+    parser = do
+      config <-
+        strOption
+          ( metavar "CONFIG"
+              <> long "config"
+              <> short 'c'
+              <> help "The configuration file to use (default: shake-fpga.yaml)"
+              <> value "shake-fpga.yaml"
+          )
+      rest <-
+        many
+          ( strArgument
+              ( metavar "ARGS"
+                  <> help "arguments to pass to shake (use -- to pass arguments with leading -)"
+              )
+          )
+      pure (config, rest)
+    opts =
+      info
+        (parser <**> helper)
+        ( fullDesc
+            <> progDesc "Build clash based FPGA projects using shake"
+        )
